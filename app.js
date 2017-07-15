c658e537038e7f4b13688b3df30b119482391422
vgalenko <vitaliygalenko@gmail.com>
2017-07-14 19:32:11 -0700

    fixed a bug

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ function openCity(evt, cityName) {
   evt.currentTarget.className += 'active';
 }
 
-// Get the element with id="defaultOpen" and click on it
+// Get the element with id="defaultOpen" and click on it //
 document.getElementById('defaultOpen').click();
 openCity();
