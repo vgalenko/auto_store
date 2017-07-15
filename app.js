5b04ea8c63621cea1ce4405b754c9a4279959ecb
vgalenko <vitaliygalenko@gmail.com>
2017-07-14 18:41:14 -0700

    added home slide out feature

--- app.js
+++ app.js
@@ -19,3 +19,21 @@ function showSlides() {
   dots[slideIndex-1].className += ' active';
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName('tabcontent');
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = 'none';
+  }
+  tablinks = document.getElementsByClassName('tablinks');
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
+  }
+  document.getElementById(cityName).style.display = 'block';
+  evt.currentTarget.className += 'active';
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById('defaultOpen').click();
+openCity();
